01f6105117febff9c43540439a303f50847ed1c7
ZavarkoZeno <zavarkoz@kkszki.hu>
2025-09-10 08:30:56 +0200

    feladat1 is ready.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,10 +6,17 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
+    public class Szemely
+    {
+        public string nev = "Peti";
+        public int kor;
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
+            Szemely tanulo1 = new Szemely();
+            Console.WriteLine($"A tanulo neve: {tanulo1.nev} és {tanulo1.kor} éves.");
         }
     }
 }
