4a48375308e1e00247d282b44da792d24d0414e1
ZavarkoZeno <zavarkoz@kkszki.hu>
2025-09-17 08:38:34 +0200

    feladat5-6-7 is ready.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,7 +8,7 @@ namespace ConsoleApp1
 {
     public class Szemely
     {
-        private string nev;
+        protected string nev;
         private int kor;
 
         public string Nev
@@ -27,16 +27,48 @@ namespace ConsoleApp1
         {
             return $"A személy neve {nev} életkora {kor}";
         }
-        /*public Szemely(string Nev, int Kor)
-        {
-            nev = Nev;
-            kor = Kor;
-        }
 
         public string Kiir()
         {
             return $"A tanuló neve: {nev} és {kor} éves.";
-        }*/
+        }
+    }
+
+    public class Bankszamla
+    {
+        private int egyenleg;
+
+        public int Egyenleg
+        {
+            get { return egyenleg; }
+            set
+            {
+                if (value >= 0)
+                    egyenleg = value;
+                else
+                    Console.WriteLine("Az érték nem lehet negatív.");
+            }
+        }
+        public void Betesz()
+        {
+
+        }
+
+        public void Kivesz()
+        {
+
+        }
+    }
+
+    public class Hallgato : Szemely
+    {
+        private int neptunkod;
+
+        public void Kiir()
+        {
+            nev = string.Empty;
+        }
+        
     }
     internal class Program
     {
@@ -47,6 +79,10 @@ namespace ConsoleApp1
             tanulo1.Kor = 23;
 
             Console.WriteLine(tanulo1);
+
+            Bankszamla bankszamla1 = new Bankszamla();
+            bankszamla1.Egyenleg = 5700;
+            Console.WriteLine(bankszamla1.Egyenleg);
         }
     }
 }
